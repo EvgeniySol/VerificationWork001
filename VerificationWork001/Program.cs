735dbb566d0c566376d12c5c45fb1d49936ff23d
«evgeny» <solov-ghenya195@mail.ru>
2022-09-18 19:53:33 +0300

    Final

--- VerificationWork001/Program.cs
+++ VerificationWork001/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] mas = new string [size];
+//string[] arr = Array.Empty<string>();
+string[] arr = new string [size];
+
+for (int i = 0; i < mas.Length; i++)
+{
+    Console.Write("Введите элемент массива №{0}:\r\n", i + 1);
+    mas[i] = Console.ReadLine();
+    if(mas[i].Length<=3)
+    {
+        arr[i]=mas[i];
+    }
+}
+
+Console.WriteLine();
+PrintArray(mas);
+Console.WriteLine();
+PrintArray(arr);
